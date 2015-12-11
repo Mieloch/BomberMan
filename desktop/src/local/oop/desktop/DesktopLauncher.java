5e8d4bc98a577658348e4f739a52f9012849ade1
Jacek Fila <filajacek@gmail.com>
2015-12-11 12:35:26 +0100

    Using LibGDX

---
+++ desktop/src/local/oop/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package local.oop.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import local.oop.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
